09f37ec9801d338dc6347f4cc252f6938d144109
Вадим Шевченко <grybefs@gmail.com>
2026-02-28 17:57:14 +0100

    feat: add Order model

--- station/models.py
+++ station/models.py
@@ -65,3 +65,14 @@ class Journey(models.Model):
 
     def __str__(self):
         return f"{self.route} at {self.departure_time}"\
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
