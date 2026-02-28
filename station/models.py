326f770095f1e1ce55226f6100b479aeef9febde
Вадим Шевченко <grybefs@gmail.com>
2026-02-28 17:54:26 +0100

    feat: add Crew model

--- station/models.py
+++ station/models.py
@@ -46,3 +46,11 @@ class Train(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
