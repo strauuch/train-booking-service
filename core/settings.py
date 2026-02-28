7497f2d32ec390c7d7fd7cd6cb56b0150a64643a
Вадим Шевченко <grybefs@gmail.com>
2026-03-01 00:26:58 +0100

    feat: add pagination

--- core/settings.py
+++ core/settings.py
@@ -141,6 +141,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
