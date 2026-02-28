0ef1e31ff2e6d16f14ac60fbf9d5898e2ccaebca
Вадим Шевченко <grybefs@gmail.com>
2026-02-28 17:47:24 +0100

    feat: add Station model

--- station/models.py
+++ station/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
