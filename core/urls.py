16591dbd1868404793a0a53b3e28daf0188a69df
Вадим Шевченко <grybefs@gmail.com>
2026-03-01 01:27:37 +0100

    chore: configure static and media file storage settings

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -39,7 +40,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
