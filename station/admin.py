98e38a920537b91d266a745390262628e6acc992
Вадим Шевченко <grybefs@gmail.com>
2026-03-01 01:21:32 +0100

    chore: configure admin panel for all applications 🧾

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Station, Route, TrainType, Train, Crew, Journey, Ticket, Order
 
-# Register your models here.
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ("name", "latitude", "longitude")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("source", "destination", "distance")
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ("name", "train_type", "cargo_num", "places_in_cargo", "capacity")
+
+
+@admin.register(Journey)
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ("route", "train", "departure_time", "arrival_time")
+    list_filter = ("route", "train")
+    filter_horizontal = ("crew",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+    list_filter = ("user", "created_at")
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "journey", "cargo", "seat", "order")
+    list_filter = ("journey",)
+
+
+admin.site.register(TrainType)
+admin.site.register(Crew)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
